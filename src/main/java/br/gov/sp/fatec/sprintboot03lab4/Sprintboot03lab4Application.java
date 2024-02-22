2e5262be1c490b7201234cdf097daa5409df240d
gabrieljssantos <gabriel.jssantos003@gmail.com>
2024-02-21 21:46:31 -0300

    First Commit

---
+++ src/main/java/br/gov/sp/fatec/sprintboot03lab4/Sprintboot03lab4Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.sprintboot03lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprintboot03lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprintboot03lab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/sprintboot03lab4/Sprintboot03lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.sprintboot03lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprintboot03lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
