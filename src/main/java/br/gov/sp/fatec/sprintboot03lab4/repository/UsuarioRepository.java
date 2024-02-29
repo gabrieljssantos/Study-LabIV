38c8ed6a41c38d85d35211174485f1b693d3591d
Gabriel Santos <gabriel.jssantos003@gmail.com>
2024-02-29 01:00:14 +0000

    Aulas Iniciais Config

--- src/main/java/br/gov/sp/fatec/sprintboot03lab4/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/sprintboot03lab4/repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
 package br.gov.sp.fatec.sprintboot03lab4.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import br.gov.sp.fatec.sprintboot03lab4.entity.Usuario;
+import br.gov.sp.fatec.sprintboot03lab4.entity.Usuario;g
 
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
        
